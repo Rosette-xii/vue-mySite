7304a143266178630a7dde77483ecf1a08e6cd91
Rosette-xiie <tpfsakura@example.com>
2022-02-19 12:28:44 +0800

    upload

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import 'aos/dist/aos.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
