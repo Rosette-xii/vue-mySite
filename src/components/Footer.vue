c2cc6d921e7ed7be1ba89f6e3e80321ad7cefd96
Rosette-xiie <tpfsakura@example.com>
2022-03-05 20:40:06 +0800

    upload

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="text-center">
     <p class="copyright fs-7">&copy; 2021 Louise Tang Projects.</p>
     <div class="go-top">
-      <a href="#"><img src="https://github.com/Rosette-xii/mySite/blob/main/assets/images/goTop.png?raw=true" alt="go to top" /></a>
+      <a href="#"><img src="../assets/images/goTop.png" alt="go to top" /></a>
     </div>
   </footer>
 </template>
